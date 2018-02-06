9b4902205f74978553fcb976d48ae7b99741eead
Dresen-Tammy <31862261+Dresen-Tammy@users.noreply.github.com>
2018-02-05 22:35:04 -0800

    Add files via upload

---
+++ js/topic6.js
@@ -0,0 +1,70 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// select all p elements where the parent is a div.
+    let select = document.getElementById('select');
+    select.innerHTML = "Inserted info goes here";
+    //select the third h3 on the page.
+    let headings = document.getElementsByTagName('h3');
+    headings[2].style.textDecoration = "underline";
+    // will select the first item with that class name.
+    let added = document.getElementsByClassName('added');
+
+    added[0].innerHTML = "Content added here.";
+    added[1].style.fontWeight = 700;
+    // add new paragraph to div.
+    let newPar = document.createElement('p');
+    newPar.innerHTML = "2. This paragraph is appended to the end of parentDiv.";
+    document.getElementById('parentDiv').appendChild(newPar);
+    // add new paragraph before the div.
+    let secondP = document.createElement('p');
+    secondP.innerHTML = "3. This paragraph is inserted before the first child of parentDiv.";
+    let parentDiv = document.getElementById('parentDiv');
+    parentDiv.insertBefore(secondP, parentDiv.childNodes[0]);
+
+    function changes() {
+        // make changes to dom when button is clicked.
+        let w = document.querySelectorAll("div > p");
+        // select multiple types of elements, (all headings)
+        let x = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+        // select element with index number or for loop
+        for (let i = 0; i < x.length; i++) {
+            x[i].style.color = "blue";
+        }
+        // select by class name
+        let y = document.querySelectorAll('.blue');
+        y[1].style.backgroundColor = "yellow";
+        // select type of element, (title of document)
+        let z = document.querySelectorAll('title');
+        w[1].innerHTML = "Document Title: " + z[0].innerHTML;
+
+    }
+    function changeColor() {
+        // change background color of body when button is clicked.
+        document.body.style.backgroundColor = "#fde6ea";
+    }
+    function removeP() {
+        // remove paragraph from parent div.
+        let parent = document.getElementById('parent');
+        let child = parent.querySelectorAll('p');
+        parent.removeChild(child[2]);
+
+    }
+    function removeP2() {
+      // identify child
+            let p2 = document.getElementById('p2');
+            // remove child
+            p2.parentNode.removeChild(p2);
+    }
+    function replaceFirstP() {
+      let p1 = document.getElementById('p1');
+      let parents = p1.parentNode;
+      let p5 = document.createElement('p');
+      p5.id = "p5";
+      p5.innerHTML = "paragraph 5";
+      parents.replaceChild(p5, p1);
+    }
