d8d5ff5c71bb93c7bbd09bd1f82f0484ba1a73c9
Dresen-Tammy <31862261+Dresen-Tammy@users.noreply.github.com>
2018-02-16 13:07:31 -0800

    Add files via upload

--- js/topic9.js
+++ js/topic9.js
@@ -27,6 +27,8 @@ function rainbow() {
 }
 dbl.addEventListener('dblclick', rainbow);
 
+
+
 // add mousedown and mouseup event that makes div appear
 const mouse = document.getElementById('mouse');
 
@@ -105,3 +107,55 @@ function down2() {
 function up2() {
     secret2.classList.remove('reveal');
 }
+// add animation event listeners to rainbow
+// add dblclick event that triggers animation
+const dbl2 = document.getElementById('dbl2');
+const bow2 = document.getElementById('bow2');
+const clouds = document.getElementsByClassName('cloud');
+function rainbow2() {
+    bow2.classList.toggle('rainbow'); 
+}
+dbl2.addEventListener('dblclick', rainbow2);
+dbl2.addEventListener('animationend', animationReset);
+function animationReset(e) {
+
+        bow2.classList.remove('rainbow');
+        for (let i = 0; i < clouds.length; i++) {
+        clouds[i].classList.remove('cloudy');
+    }
+}
+
+
+dbl2.addEventListener('animationstart', addClouds);
+dbl2.addEventListener('animationiteration', addClouds2);
+
+function addClouds() {
+    clouds[0].classList.add('cloudy');
+}
+function addClouds2(e) {
+    console.log(e.elapsedTime);
+    if (e.elapsedTime <= 2) {
+        clouds[1].classList.add('cloudy');
+    } else {
+        clouds[2].classList.add('cloudy');
+    }
+}
+
+// add touch events
+var dragArea = document.getElementById('dragArea');
+var dragBox = document.getElementById('dragBox');
+dragArea.addEventListener('touchstart', function(e) {
+    dragBox.src = 'lion.jpg';
+});
+dragArea.addEventListener('touchmove', function(e) {
+    dragBox.src = 'kittencrouch.jpg';
+});
+dragArea.addEventListener('touchend', function(e) {
+    dragBox.src = 'kitten.jpg';
+});
+dragBox.addEventListener('touchenter', function(e) {
+    dragBox.src = 'catinhat.jpg';
+});
+dragBox.addEventListener('touchleave'), function(e) {
+    dragBox.src = 'kitten.jpg';
+};
